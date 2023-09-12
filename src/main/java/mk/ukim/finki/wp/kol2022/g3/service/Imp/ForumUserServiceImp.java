714b04a1159fa7f89c1feeba6fafd500a70dcf3f
DamjanSimonovski <damjan.simonovski.99@gmail.com>
2023-09-13 01:09:43 +0200

    Added my project

---
+++ src/main/java/mk/ukim/finki/wp/kol2022/g3/service/Imp/ForumUserServiceImp.java
@@ -0,0 +1,68 @@
+package mk.ukim.finki.wp.kol2022.g3.service.Imp;
+
+import mk.ukim.finki.wp.kol2022.g3.model.ForumUser;
+import mk.ukim.finki.wp.kol2022.g3.model.ForumUserType;
+import mk.ukim.finki.wp.kol2022.g3.model.Interest;
+import mk.ukim.finki.wp.kol2022.g3.model.exceptions.InvalidForumUserIdException;
+import mk.ukim.finki.wp.kol2022.g3.repository.ForumUserRepository;
+import mk.ukim.finki.wp.kol2022.g3.repository.InterestRepository;
+import mk.ukim.finki.wp.kol2022.g3.service.ForumUserService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class ForumUserServiceImp implements ForumUserService {
+    private final ForumUserRepository forumUserRepository;
+    private final InterestRepository interestRepository;
+
+    public ForumUserServiceImp(ForumUserRepository forumUserRepository, InterestRepository interestRepository) {
+        this.forumUserRepository = forumUserRepository;
+        this.interestRepository = interestRepository;
+    }
+
+    @Override
+    public List<ForumUser> listAll() {
+        return this.forumUserRepository.findAll();
+    }
+
+    @Override
+    public ForumUser findById(Long id) {
+        return this.forumUserRepository.findById(id).orElseThrow(InvalidForumUserIdException::new);
+    }
+
+    @Override
+    public ForumUser create(String name, String email, String password, ForumUserType type, List<Long> interestId, LocalDate birthday) {
+        List<Interest> interests = this.interestRepository.findAllById(interestId);
+        ForumUser forumUser = new ForumUser(name, email, password, type, interests, birthday);
+        return this.forumUserRepository.save(forumUser);
+    }
+
+    @Override
+    public ForumUser update(Long id, String name, String email, String password, ForumUserType type, List<Long> interestId, LocalDate birthday) {
+        ForumUser forumUser = this.findById(id);
+        forumUser.setName(name);
+        forumUser.setEmail(email);
+        forumUser.setPassword(password);
+        forumUser.setType(type);
+        forumUser.setBirthday(birthday);
+
+        List<Interest> interests = this.interestRepository.findAllById(interestId);
+        forumUser.setInterests(interests);
+
+        return this.forumUserRepository.save(forumUser);
+    }
+
+    @Override
+    public ForumUser delete(Long id) {
+        ForumUser forumUser = this.findById(id);
+        this.forumUserRepository.delete(forumUser);
+        return forumUser;
+    }
+
+    @Override
+    public List<ForumUser> filter(Long interestId, Integer age) {
+        return null;
+    }
+}
